9e21bbe42a0b98aadec91c92992c77ed397d8b5e
Jungyeol Kim <jungyeolkim@JungyeolnoMacBook-Pro.local>
2019-05-16 20:45:13 +0900

    ㅍㅏ일 추가

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+import "../scss/styles.scss";
+import $ from "jquery";
+
+$(function() {
+  console.log("testtest");
+
+});
+
+// const a = () => {
+//   let a = 99999;
+//   console.log(`7666${a}`);
+// }
+
+// a();
+
+// serverless
+
+
+// axios.get('22.344.33.22/api/user/5').then((data) => {
+//   console.log(data);
+//   $(#aaa).append(name.kim);
+// })
+
+// {
+//   name: 'kim',
+//   id: 1
+// }
+
+// class AA {
+//   constructor() {
+//     this.a = 11;
+//     this.a = 11;
+//     this.a = 11;
+//     this.a = 11;
+//   }
+
+//   b() {
+//     console.log(this.a);
+//   }
+// }
+
+// const a = new AA();
+
+// a.b();
